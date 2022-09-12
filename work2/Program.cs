18fed286fd1963f2dd717a6f92ba7e3bdb68b89e
Alexander Diakov <alex.dkv@yandex.ru>
2022-09-12 16:52:46 +0300

    added folders and code

---
+++ work2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int tempNumber = 0;
+    int digitNumber = 0;
+    for (int i = 0; number > 0; i++)
+    {
+        digitNumber = number % 10;
+        tempNumber = tempNumber + digitNumber;
+        number = number / 10;
+    }
+    return tempNumber;
+}
+Console.WriteLine($"сумма цифр в числе --> " + SumNumber(number));
+
---
+++ work3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Массив");
+int[] CreateArray()
+{
+    Random randomNum = new Random();
+    int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randomNum.Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+}
+int[] array = CreateArray();
+PrintArray(array);
+
+
+//Ниже пример с ручным вводом массива
+
+
+
+/* void CreateArray(string[] args)
+{
+    Console.WriteLine("Введите размер массива ");
+    int countElements = int.Parse(Console.ReadLine());
+    int[] array = new int[countElements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива {i}:\t");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("Вывод массива ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+CreateArray(args); */
+
